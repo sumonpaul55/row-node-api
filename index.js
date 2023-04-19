10fa77415602a18c6964589b1a6043fa80805415
sumon paul <sumpaul100@gmail.com>
2023-04-20 02:13:02 +0600

    server created and some requirements done

--- index.js
+++ index.js
@@ -6,3 +6,25 @@
  *
  */
 
+// depandecies
+const http = require("http");
+// app objects or module scafolding
+const app = {};
+
+//configerationa
+app.config = {
+  port: 3000,
+};
+
+//create server
+app.createServer = () => {
+  const server = http.createServer(handleRequestRes);
+  server.listen(app.config.port, () => {
+    console.log(`listening to ${app.config.port}`);
+  });
+};
+//handle request and handle response
+const handleRequestRes = (req, res) => {
+  //response handlers
+  res.end("hello world");
+};
