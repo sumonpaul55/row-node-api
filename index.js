88a3b7921e32a347b6a45c24938ec3bc13e597ce
sumon paul <sumpaul100@gmail.com>
2023-04-23 02:19:12 +0600

    url parsed, trimed, method, headers and decoder data get body get post method etc.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 // depandecies
 const http = require("http");
 const url = require("url");
+const { StringDecoder } = require("string_decoder");
 // app objects or module scafolding
 const app = {};
 
@@ -35,9 +36,26 @@ app.handleRequestRes = (req, res) => {
   // console.log(trimedPath);/
   //method name
   const method = req.method.toLowerCase();
-  console.log(method);
+  // console.log(method);
+  // query string
+  const queryStringObject = parsedUrl.query;
+  console.log(parsedUrl);
+  //headers //header is a meta data of url
+  const headersObject = req.headers; //header is situated in req.headers it's an object
+  console.log(headersObject);
+
+  //realdata received from server using string decoder
+  const decoder = new StringDecoder("utf8");
+  let realData = "";
+  req.on("data", (buffer) => {
+    realData += decoder.write(buffer);
+  });
+  req.on("end", () => {
+    realData += decoder.end();
+    console.log(realData);
+    res.end("something is better than nothing. Yes");
+  });
   //response handlers
-  res.end("something is better than nothing. Yes");
   //
 };
 
