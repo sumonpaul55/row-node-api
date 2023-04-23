eca16d34bd26db7de038c8030dfc2a3ad7994590
sumon paul <sumpaul100@gmail.com>
2023-04-23 18:03:17 +0600

    working on routing

---
+++ handlers/routesHandler/sampleHandler.js
@@ -0,0 +1,12 @@
+// title: sample handleReqRes
+// description : sample handleReqRes
+// Author: sumon chandra paul
+//Date: 23/04/2023
+//Module scafolding
+
+const handler = {};
+handler.samplehandler = () => {
+  console.log("sample handler");
+};
+
+module.exports = handler;
---
+++ route.js
@@ -0,0 +1,10 @@
+// title : application routes
+// description: application routes control Headers
+// Author: sumon paul
+
+//dependencies
+const { samplehandler } = require("./handlers/routesHandler/sampleHandler");
+const sample = require("./handlers/routesHandler/sampleHandler");
+const routes = {
+  smaple: samplehandler,
+};
